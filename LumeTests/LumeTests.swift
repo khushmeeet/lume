a4155572d3658e4f6a21e0b7b62c4463206f45db
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2025-02-25 23:02:57 -0800

    Initial Commit

---
+++ LumeTests/LumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumeTests.swift
+//  LumeTests
+//
+//  Created by Khushmeet Singh on 2/25/25.
+//
+
+import Testing
+@testable import Lume
+
+struct LumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
